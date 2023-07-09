05884414a3ce1b32cd1b062c7538175577b0b588
yanivthebar <yanivb527@gmail.com>
2023-07-09 18:05:04 +0300

    dice and hebrew

--- src/routes/characterSheet.jsx
+++ src/routes/characterSheet.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import playerCharacters from "../data/playerCharacters.json";
 import dndFormulas from "../scripts/dndFormulas";
@@ -10,7 +11,12 @@ export default function CharacterSheet() {
 
     let charId = useParams().id;
     let characterSheet = playerCharacters[charId];
+    const [diceRoll, setdiceRoll] = useState('');
 
+    function rollAttack(modifier) {
+        let roll = dndFormulas.rollAttack(modifier)
+        setdiceRoll(' מתקפה! גלגת ' + roll.roll + ' לסה"כ של ' + roll.rollTotal)
+    }
 
     return (
         <>
@@ -22,17 +28,46 @@ export default function CharacterSheet() {
         </section>
 
         <section className="character-attributes">
-            <ul>
+            <ul className="attribute-list">
             {Object.keys(characterSheet.attributes).map(attributeName => {
                 return <li>
                     <div className="attribute-name">{attributeName}</div>
-                    {console.log(characterSheet.attributes[attributeName])}
                     <div className="attribute-value">{characterSheet.attributes[attributeName]}</div>
                     <div className="attribute-mod">{ dndFormulas.calcAttributeModifier(characterSheet.attributes[attributeName])}</div>
                 </li>
             })}
             </ul>   
+        </section>
 
+        <section className="character-stasuses">
+            <div className="defensive-stats">
+                <div className="hitpoints">
+                    נקודות פגיעה: {characterSheet.hitPoints}
+                </div>
+                <div className="hitpoints">
+                    ערך הגנתי: {characterSheet.armorClass}
+                </div>
+                <div className="hitpoints">
+                    קוביית שיקום: 1ק{characterSheet.hitDie}
+                </div>
+            </div>
+        </section>
+
+        <section  className="character-attacks">
+            {characterSheet.attacks.map(attack => {
+                return <div class="attackRoll">
+                    <div className="attack-name">{attack.name}</div>
+                    <div className="attack-roll">קוביית 1ק20 בעוד {attack.attackRollBonus}</div>
+                    <button className="attack-button" onClick={() => rollAttack(attack.attackRollBonus)}>!תקוף</button>
+                </div>
+            })}
+        </section>
+
+        <section className="dice-rolls-container">
+            <h1> גלגולים!</h1>
+            <div className="rolls">
+                {diceRoll}
+            </div>
         </section>
 
         </grid>
--- src/scripts/dndFormulas.js
+++ src/scripts/dndFormulas.js
@@ -1,7 +1,26 @@
 export default {
     calcAttributeModifier,
+    rollAttack
 }
 
 function calcAttributeModifier(attributeVal) {
     return Math.floor((attributeVal - 10) / 2);
+}
+
+function rollAttack(attackModifier) {
+ let roll =  _roll1d20();
+ let rollTotal = roll + attackModifier
+    
+ return {
+    roll,
+    rollTotal
+ }
+}
+
+function rollDamage(damageModifier) {
+
+}
+
+function _roll1d20() {
+    Math.floor(1 + Math.random() * 20);
 }
